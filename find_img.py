99834968791aff8b9708f84f9f8872dd36a5c4f6
maxthiessen92 <sthiessen25@amherst.edu>
2024-11-18 15:30:47 -0500

    Update find_img.py

--- find_img.py
+++ find_img.py
@@ -1,18 +1,19 @@
 import numpy as np
 import imageio
 import sys
-if __name__ == "__main__":
-    img = imageio.imread(sys.argv[1])
+
+def find_img(i, h, w, depth, rd, gr, bl, orient, color_ord, folder):
+    img = imageio.imread(i)
     height, width, channels = img.shape
     print("Height:", height, "Width:", width, "Number of Channels:", channels)
-    lsbs = (1 << int(sys.argv[4])) - 1
+    lsbs = (1 << int(depth)) - 1
     bits = ""
-    red = bool(int(sys.argv[5]))
-    green = bool(int(sys.argv[6]))
-    blue = bool(int(sys.argv[7]))
-    orientation = sys.argv[8].lower()
-    color_order = sys.argv[9].lower()
-    bits_needed = (int(sys.argv[2]) * int(sys.argv[3]) * 3 * 8 + 64)
+    red = bool(int(rd))
+    green = bool(int(gr))
+    blue = bool(int(bl))
+    orientation = orient.lower()
+    color_order = color_ord.lower()
+    bits_needed = (int(h) * int(w) * 3 * 8 + 64)
     print("bits needed: ", bits_needed)
     if orientation == "lrtb":
         for r in range(height):
@@ -21,15 +22,15 @@ if __name__ == "__main__":
                     if len(bits) == bits_needed:
                         break
                     if red:
-                        bits += (bin((img[r,c,0] & lsbs))[2:].zfill(int(sys.argv[4])))
+                        bits += (bin((img[r,c,0] & lsbs))[2:].zfill(int(depth)))
                     if len(bits) == bits_needed:
                         break
                     if green:
-                        bits+=(bin((img[r,c,1] & lsbs))[2:].zfill(int(sys.argv[4])))
+                        bits+=(bin((img[r,c,1] & lsbs))[2:].zfill(int(depth)))
                     if len(bits) == bits_needed:
                         break
                     if blue:
-                        bits+=(bin((img[r,c,2] & lsbs))[2:].zfill(int(sys.argv[4])))
+                        bits+=(bin((img[r,c,2] & lsbs))[2:].zfill(int(depth)))
     elif orientation == "tblr":
         for c in range(height):
             for r in range(width):
@@ -37,15 +38,15 @@ if __name__ == "__main__":
                     if len(bits) == bits_needed:
                         break
                     if red:
-                        bits+=(bin((img[r,c,0] & lsbs))[2:].zfill(int(sys.argv[4])))
+                        bits+=(bin((img[r,c,0] & lsbs))[2:].zfill(int(depth)))
                     if len(bits) == bits_needed:
                         break
                     if green:
-                        bits+=(bin((img[r,c,1] & lsbs))[2:].zfill(int(sys.argv[4])))
+                        bits+=(bin((img[r,c,1] & lsbs))[2:].zfill(int(depth)))
                     if len(bits) == bits_needed:
                         break
                     if blue:
-                        bits+=(bin((img[r,c,2] & lsbs))[2:].zfill(int(sys.argv[4])))
+                        bits+=(bin((img[r,c,2] & lsbs))[2:].zfill(int(depth)))
     elif orientation == "btlr":
         for c in range(height-1, -1, -1):
             for r in range(width):
@@ -53,15 +54,15 @@ if __name__ == "__main__":
                     if len(bits) == bits_needed:
                         break
                     if red:
-                        bits+=(bin((img[r,c,0] & lsbs))[2:].zfill(int(sys.argv[4])))
+                        bits+=(bin((img[r,c,0] & lsbs))[2:].zfill(int(depth)))
                     if len(bits) == bits_needed:
                         break
                     if green:
-                        bits+=(bin((img[r,c,1] & lsbs))[2:].zfill(int(sys.argv[4])))
+                        bits+=(bin((img[r,c,1] & lsbs))[2:].zfill(int(depth)))
                     if len(bits) == bits_needed:
                         break
                     if blue:
-                        bits+=(bin((img[r,c,2] & lsbs))[2:].zfill(int(sys.argv[4])))
+                        bits+=(bin((img[r,c,2] & lsbs))[2:].zfill(int(depth)))
     elif orientation == "rlbt":
         for c in range(height-1, -1, -1):
             for r in range(width-1, -1, -1):
@@ -69,20 +70,20 @@ if __name__ == "__main__":
                     if len(bits) == bits_needed:
                         break
                     if red:
-                        bits+=(bin((img[r,c,0] & lsbs))[2:].zfill(int(sys.argv[4])))
+                        bits+=(bin((img[r,c,0] & lsbs))[2:].zfill(int(depth)))
                     if len(bits) == bits_needed:
                         break
                     if green:
-                        bits+=(bin((img[r,c,1] & lsbs))[2:].zfill(int(sys.argv[4])))
+                        bits+=(bin((img[r,c,1] & lsbs))[2:].zfill(int(depth)))
                     if len(bits) == bits_needed:
                         break
                     if blue:
-                        bits+=(bin((img[r,c,2] & lsbs))[2:].zfill(int(sys.argv[4])))
+                        bits+=(bin((img[r,c,2] & lsbs))[2:].zfill(int(depth)))
     bits = bits[64:]
     print("Number of bits: ", len(bits)+64)
-    image = np.zeros((int(sys.argv[2]), int(sys.argv[3]), 3), dtype=np.uint8)
-    for r in range(int(sys.argv[2])):
-        for c in range(int(sys.argv[3])):
+    image = np.zeros((int(h), int(w), 3), dtype=np.uint8)
+    for r in range(int(h)):
+        for c in range(int(w)):
             if color_order == "rgb":
                 if len(bits) >= 8:
                     image[r, c][0] = int(bits[:8], 2)
@@ -116,5 +117,6 @@ if __name__ == "__main__":
                 else:
                     image[r, c][0] = 0
 
-    imageio.imwrite("Hidden/"+sys.argv[1][7:], image)
-                           
+    imageio.imwrite(folder+"/"+i[7:], image)
+if __name__ == "__main__":
+    find_img(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8], sys.argv[9], sys.argv[10])
