beb0eb06e668836868f99f3558d6f8c963f1105d
maxthiessen92 <sthiessen25@amherst.edu>
2024-11-18 20:40:57 -0500

    Update find_img.py
    Adjustments so 2nd, 3rd, etc. lsb alone can be extracted

--- find_img.py
+++ find_img.py
@@ -2,18 +2,18 @@ import numpy as np
 import imageio
 import sys
 
-def find_img(i, h, w, depth, rd, gr, bl, orient, color_ord, folder):
+def find_img(i, h, w, depth, rd, gr, bl, orient, color_ord, folder, start_bit):
     img = imageio.imread(i)
     height, width, channels = img.shape
     print("Height:", height, "Width:", width, "Number of Channels:", channels)
-    lsbs = (1 << int(depth)) - 1
+    lsbs = ((1 << int(depth)) - 1) << (int(start_bit)-1)
     bits = ""
     red = bool(int(rd))
     green = bool(int(gr))
     blue = bool(int(bl))
     orientation = orient.lower()
     color_order = color_ord.lower()
-    bits_needed = (int(h) * int(w) * 3 * 8 + 64)
+    bits_needed = ((int(h) * int(w) * 3 * 8) + 64)
     print("bits needed: ", bits_needed)
     if orientation == "lrtb":
         for r in range(height):
@@ -22,15 +22,15 @@ def find_img(i, h, w, depth, rd, gr, bl, orient, color_ord, folder):
                     if len(bits) == bits_needed:
                         break
                     if red:
-                        bits += (bin((img[r,c,0] & lsbs))[2:].zfill(int(depth)))
+                        bits += (bin(((img[r,c,0] & lsbs) >> (int(start_bit)-1)))[2:].zfill(int(depth)))
                     if len(bits) == bits_needed:
                         break
                     if green:
-                        bits+=(bin((img[r,c,1] & lsbs))[2:].zfill(int(depth)))
+                        bits += (bin(((img[r,c,1] & lsbs) >> (int(start_bit)-1)))[2:].zfill(int(depth)))
                     if len(bits) == bits_needed:
                         break
                     if blue:
-                        bits+=(bin((img[r,c,2] & lsbs))[2:].zfill(int(depth)))
+                        bits += (bin(((img[r,c,2] & lsbs) >> (int(start_bit)-1)))[2:].zfill(int(depth)))
     elif orientation == "tblr":
         for c in range(height):
             for r in range(width):
@@ -38,15 +38,15 @@ def find_img(i, h, w, depth, rd, gr, bl, orient, color_ord, folder):
                     if len(bits) == bits_needed:
                         break
                     if red:
-                        bits+=(bin((img[r,c,0] & lsbs))[2:].zfill(int(depth)))
+                        bits += (bin(((img[r,c,0] & lsbs) >> (int(start_bit)-1)))[2:].zfill(int(depth)))
                     if len(bits) == bits_needed:
                         break
                     if green:
-                        bits+=(bin((img[r,c,1] & lsbs))[2:].zfill(int(depth)))
+                        bits += (bin(((img[r,c,1] & lsbs) >> (int(start_bit)-1)))[2:].zfill(int(depth)))
                     if len(bits) == bits_needed:
                         break
                     if blue:
-                        bits+=(bin((img[r,c,2] & lsbs))[2:].zfill(int(depth)))
+                        bits += (bin(((img[r,c,2] & lsbs) >> (int(start_bit)-1)))[2:].zfill(int(depth)))
     elif orientation == "btlr":
         for c in range(height-1, -1, -1):
             for r in range(width):
@@ -54,15 +54,15 @@ def find_img(i, h, w, depth, rd, gr, bl, orient, color_ord, folder):
                     if len(bits) == bits_needed:
                         break
                     if red:
-                        bits+=(bin((img[r,c,0] & lsbs))[2:].zfill(int(depth)))
+                        bits += (bin(((img[r,c,0] & lsbs) >> (int(start_bit)-1)))[2:].zfill(int(depth)))
                     if len(bits) == bits_needed:
                         break
                     if green:
-                        bits+=(bin((img[r,c,1] & lsbs))[2:].zfill(int(depth)))
+                        bits += (bin(((img[r,c,1] & lsbs) >> (int(start_bit)-1)))[2:].zfill(int(depth)))
                     if len(bits) == bits_needed:
                         break
                     if blue:
-                        bits+=(bin((img[r,c,2] & lsbs))[2:].zfill(int(depth)))
+                        bits += (bin(((img[r,c,2] & lsbs) >> (int(start_bit)-1)))[2:].zfill(int(depth)))
     elif orientation == "rlbt":
         for c in range(height-1, -1, -1):
             for r in range(width-1, -1, -1):
@@ -70,15 +70,15 @@ def find_img(i, h, w, depth, rd, gr, bl, orient, color_ord, folder):
                     if len(bits) == bits_needed:
                         break
                     if red:
-                        bits+=(bin((img[r,c,0] & lsbs))[2:].zfill(int(depth)))
+                        bits += (bin(((img[r,c,0] & lsbs) >> (int(start_bit)-1)))[2:].zfill(int(depth)))
                     if len(bits) == bits_needed:
                         break
                     if green:
-                        bits+=(bin((img[r,c,1] & lsbs))[2:].zfill(int(depth)))
+                        bits += (bin(((img[r,c,1] & lsbs) >> (int(start_bit)-1)))[2:].zfill(int(depth)))
                     if len(bits) == bits_needed:
                         break
                     if blue:
-                        bits+=(bin((img[r,c,2] & lsbs))[2:].zfill(int(depth)))
+                        bits += (bin(((img[r,c,2] & lsbs) >> (int(start_bit)-1)))[2:].zfill(int(depth)))
     bits = bits[64:]
     print("Number of bits: ", len(bits)+64)
     image = np.zeros((int(h), int(w), 3), dtype=np.uint8)
@@ -117,6 +117,6 @@ def find_img(i, h, w, depth, rd, gr, bl, orient, color_ord, folder):
                 else:
                     image[r, c][0] = 0
 
-    imageio.imwrite(folder+"/"+i[7:], image)
+    imageio.imwrite(folder+"/"+i.partition("/")[-1], image)
 if __name__ == "__main__":
-    find_img(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8], sys.argv[9], sys.argv[10])
+    find_img(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8], sys.argv[9], sys.argv[10], sys.argv[11])
